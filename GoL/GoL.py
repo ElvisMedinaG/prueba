333717cccec95b677421e22e20dc1b49f8f4665d
ElvisMedinaG <elvismedina1999@gmail.com>
2020-06-18 23:37:21 +0200

    ssss

--- GoL/GoL.py
+++ GoL/GoL.py
@@ -1,7 +1,7 @@
 import pygame
 import numpy as np
 
-pygame.init ()
+pygame.init()
 
 width, height = 1000, 1000
 screen = pygame.display.set_mode((height, width))
@@ -9,5 +9,29 @@ screen = pygame.display.set_mode((height, width))
 bg = 25, 25, 25
 screen.fill(bg)
 
+nxC, nyC = 25, 25
+
+dimCW = width / nxC
+dimCH = height / nyC
+
+gameState = np.zeros((nxC, nyC))
+
 while True:
-    pass
+    
+    for y in range(0, nxC):
+        for x in range(0, nyC):
+
+            n_neigh = gameState[(x-1), (y-1)] 
+
+            poly = [((x) * dimCW, y * dimCH), 
+            ((x+1) * dimCW, y * dimCH), 
+            ((x+1) * dimCW, (y+1) * dimCH), 
+            ((x) * dimCW, (y+1) * dimCH)]
+            
+        
+            
+            
+            pygame.draw.polygon(screen, (128, 128, 128), poly, 1)
+
+
+pygame.display.flip()
